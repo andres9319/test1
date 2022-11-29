d09d25cfdd529473ef6abc3bffed23c777040e21
Andres Aravena <andres.fal@hotmail.es>
2022-11-29 05:50:30 -0300

    realse 1

---
+++ precache-manifest.1ae6d63eca50bd21e7ec9e1a2e0ec078.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "1845b2beceec4e923be8",
+    "url": "/static/js/main.1845b2be.chunk.js"
+  },
+  {
+    "revision": "8e10d1cd40b785c621c8",
+    "url": "/static/js/1.8e10d1cd.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4033eb9a8126634798b3d7ec7620bdc6",
+    "url": "/index.html"
+  }
+];
